be6b4af134c52515afaadccde495aae97cd670f1
Mohitkumar Vora <mohitv88@gmail.com>
2024-05-30 12:08:47 +0530

    update customlogger file

--- Utilities/customLogger.py
+++ Utilities/customLogger.py
@@ -1,28 +1,11 @@
 import logging
-import os
+
 
 class LogGen:
     @staticmethod
-    def loggen(log_level=logging.INFO):
-        logger = logging.getLogger("automationLogger")
-
-        # Check if the logger already has handlers to prevent duplicate logs
-        if not logger.hasHandlers():
-            log_directory = ".\\Logs"
-            if not os.path.exists(log_directory):
-                os.makedirs(log_directory)
-
-            logging.basicConfig(filename=os.path.join(log_directory, "automation.log"),
-                                format='%(asctime)s: %(levelname)s: %(message)s',
-                                datefmt='%m/%d/%Y %I:%M:%S %p',
-                                level=log_level)
-
-            # Creating a console handler for output to the console
-            console_handler = logging.StreamHandler()
-            console_handler.setLevel(log_level)
-            console_handler.setFormatter(
-                logging.Formatter('%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))
-            logger.addHandler(console_handler)
-
-        logger.setLevel(log_level)
+    def loggen():
+        logging.basicConfig(filename=".\\Logs\\automation.log",
+                            format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
         return logger
